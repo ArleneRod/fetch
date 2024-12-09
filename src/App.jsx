7239be6a58b39c98abbb0eeda7276d3620c10b46
ArleneRod <arlene.vv@protonmail.com>
2024-12-09 16:18:08 +0100

    Filter by Term

--- src/App.jsx
+++ src/App.jsx
@@ -27,15 +27,15 @@ const Course = ({ course }) => (
   </div>
 );
 
-const TermButton = ({ term, selectedTerm, setSelectedTerm }) => (
+const TermButton = ({ term, setTerm, checked }) => (
   <>
     <input
       type="radio"
       id={term}
       className="btn-check"
+      checked={checked}
       autoComplete="off"
-      checked={term === selectedTerm}
-      onChange={() => setSelectedTerm(term)}
+      onChange={() => setTerm(term)}
     />
     <label className="btn btn-success m-1 p-2" htmlFor={term}>
       {term}
@@ -43,31 +43,31 @@ const TermButton = ({ term, selectedTerm, setSelectedTerm }) => (
   </>
 );
 
-const TermSelector = ({ selectedTerm, setSelectedTerm }) => (
+const TermSelector = ({ term, setTerm }) => (
   <div className="btn-group">
-    {Object.values(terms).map((term) => (
+    {Object.values(terms).map((value) => (
       <TermButton
-        key={term}
-        term={term}
-        selectedTerm={selectedTerm}
-        setSelectedTerm={setSelectedTerm}
+        key={value}
+        term={value}
+        setTerm={setTerm}
+        checked={value === term}
       />
     ))}
   </div>
 );
 
 const CourseList = ({ courses }) => {
-  const [selectedTerm, setSelectedTerm] = React.useState("Fall");
+  const [term, setTerm] = React.useState("Fall");
 
-  const filteredCourses = Object.values(courses).filter(
-    (course) => course.term === selectedTerm
+  const termCourses = Object.values(courses).filter(
+    (course) => course.term === term
   );
 
   return (
     <>
-      <TermSelector selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm} />
+      <TermSelector term={term} setTerm={setTerm} />
       <div className="course-list">
-        {filteredCourses.map((course) => (
+        {termCourses.map((course) => (
           <Course key={course.id} course={course} />
         ))}
       </div>
